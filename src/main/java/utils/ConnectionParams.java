a22aacdb017a341f95800e4d940d3e3b15cf140f
Edoardo Baral <edoardo@baral.it>
2019-10-20 13:23:20 +0200

    Aggiunta classe con stringhe statiche per parametri di connessione a MongoDB

---
+++ src/main/java/utils/ConnectionParams.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class ConnectionParams
+{
+	public static final String URI = "mongodb+srv://EdoardoBaral:r5_m6_v7_e9@cluster0-h2qqj.gcp.mongodb.net/test?retryWrites=true&w=majority";
+	public static final String DATABASE = "test_library";
+}
